e27e05f9353ecb9dbf33dd93645928bfb2f6b82a
brnklsnn <borna.klisanin@proton.me>
2025-07-28 20:54:24 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = "satnica-v1";
+const URLS_TO_CACHE = [
+  "/Satnica/",
+  "/Satnica/index.html",
+  "/Satnica/app.js",
+  "/Satnica/style.css",
+  "/Satnica/ikone/icon-192.png",
+  "/Satnica/ikone/icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
